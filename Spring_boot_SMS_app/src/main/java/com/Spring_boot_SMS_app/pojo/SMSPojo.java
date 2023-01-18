97d9a9098196319f21334fa9a949b7a30b1c5ac0
skillrajkumar <117907936+skillrajkumar@users.noreply.github.com>
2023-01-18 18:34:08 +0530

    Add files via upload

---
+++ Spring_boot_SMS_app/src/main/java/com/Spring_boot_SMS_app/pojo/SMSPojo.java
@@ -0,0 +1,21 @@
+package com.Spring_boot_SMS_app.pojo;
+
+public class SMSPojo {
+
+	private String to;
+	private String message;
+	
+	
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ Spring_boot_SMS_app/src/test/java/com/Spring_boot_SMS_app/SpringBootSmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_boot_SMS_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
